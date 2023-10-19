db8d8d4fe946fccc67396012997fa5447ef00275
alonahaletska <al.khaletskaya@gmail.com>
2023-10-19 16:47:16 +0300

    practica

---
+++ pract.js
@@ -0,0 +1,17 @@
+// Оголоси дві змінні, productName для назви товару, і pricePerItem для зберігання ціни за штуку.
+//При оголошенні надай змінним наступні значення:
+// назва - рядок "Droid";
+// ціна за штуку - число 2000.
+
+// const productName = "Droid";
+// const pricePerItem = 2000;
+
+// Ім'я товару змінили на "Repair droid" і збільшили його ціну на 1500 кредитів. 
+// Перевизнач значення змінних pricePerItem і productName після їх оголошення.
+
+let productName = "Droid";
+let pricePerItem = 2000;
+
+productName = "Repair droid";
+pricePerItem = 3500;
+ console.log(productName);
