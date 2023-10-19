c02dbfab8346658a90ee651514d3aa7dcf9db88e
alonahaletska <al.khaletskaya@gmail.com>
2023-10-19 16:25:39 +0300

    логічні оператори, математичні операції

---
+++ script.js
@@ -0,0 +1,106 @@
+// обьявления переменных
+const a = 2;//инициализованная переменная 
+let a;//не инициализованная переменная т.к а не присвоено значение
+
+//проверка типа данных
+let username;
+console.log(typeof username); // "undefined"
+
+const quantity = 17;
+console.log(typeof quantity);
+
+
+//вывод по запросу 
+const message = "JavaScript is awesome!";
+console.log(message);
+
+const message = "JavaScript is awesome!";
+alert(message);
+
+//получение данных
+
+// Просимо клієнта підтвердити бронювання готелю
+// і зберігаємо результат роботи confirm у змінну
+const isComing = confirm("Please confirm hotel reservation");
+console.log(isComing);
+
+
+// Запитуємо назву готеля, в якому хотів би зупинитися клієнт
+// і зберігаємо результат виклику prompt у змінну.
+const hotelName = prompt("Please enter desired hotel name");
+console.log(hotelName);
+
+
+//математические операции
+const x = 10;
+const y = 5;
+
+// Додавання
+console.log(x + y); // 15
+
+// Віднімання
+console.log(x - y); // 5
+
+// Множення
+console.log(x * y); // 50
+
+// Ділення
+console.log(x / y); // 2
+
+// Остача від ділення
+console.log(x % y); // 0
+
+// Додавання із заміною (також є для всіх інших операторів)
+let value = 5;
+
+// Аналогічно запису value = value + 10;
+value += 10;
+console.log(value); // 15
+
+
+
+//операторы сравнения
+const x = 5;
+const y = 10;
+const z = 5;
+
+console.log("x > y:", x > y); // false
+console.log("x < y:", x < y); // true
+console.log("x < z:", x < z); // false
+console.log("x <= z:", x <= z); // true
+console.log("x === y:", x === y); // false
+console.log("x === z:", x === z); // true
+console.log("x !== y:", x !== y); // true
+console.log("x !== z:", x !== z); // false
+
+
+//логические операции
+
+//Логічне «І» Оператор && приводить всі операнди до буля і повертає значення одного з них. Лівий операнд - якщо його можна привести до false, і правий - в інших випадках.
+console.log(1 && 5); // true && true -> 5
+console.log(5 && 1); // true && true -> 1
+console.log(0 && 2); // false && true -> 0
+console.log(2 && 0); // true && false -> 0
+console.log("" && "Mango"); // false && true -> ""
+console.log("Mango" && ""); // true && false -> ""
+console.log("Mango" && "Poly"); // true && true -> "Poly"
+console.log("Poly" && "Mango"); // true && true -> "Mango"
+
+//Логічне «АБО Оператор || приводить всі операнди до буля і повертає значення одного з них. Лівий операнд - якщо його можна привести до true, і правий - в інших випадках.
+console.log(true || false); // true
+console.log(false || true); // true
+console.log(true || true); // true
+console.log(3 || false); // 3
+console.log(false || 3); // 3
+console.log(3 || true); // 3
+console.log(true || 3); // true
+
+//Логічне «НЕ» Всі оператори, які ми розглядали до цього, були бінарними - містять два операнди: лівий і правий. Логічне «НЕ» - це унарний оператор, який виконує операцію над одним операндом з правої сторони.
+console.log(!true); // false
+console.log(!false); // true
+console.log(!3); // !3 -> !true -> false
+console.log(!"Mango"); // !"Mango" -> !true -> false
+console.log(!0); // !0 -> !false -> true
+console.log(!""); // !"" -> !false -> true
+const isOnline = true;
+const isNotOnline = !isOnline; // !isOnline -> !true -> false
